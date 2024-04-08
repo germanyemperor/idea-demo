4443905d342dbb2a25b5f25bbeca6163756c6676
cbh <1198837065@qq.com>
2024-04-08 17:37:11 +0800

    hi idea

--- src/main/java/com/atguigu/Hello.java
+++ src/main/java/com/atguigu/Hello.java
@@ -6,5 +6,6 @@ package com.atguigu;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word!!!");
+        System.out.println("hi idea");
     }
 }
