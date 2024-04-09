26acd734a853db90ac57812688bbcba3cac78f8c
cbh <1198837065@qq.com>
2024-04-09 14:12:42 +0800

    完成模块2的开发

--- src/main/java/com/atguigu/Module2.java
+++ src/main/java/com/atguigu/Module2.java
@@ -6,5 +6,6 @@ package com.atguigu;
 public class Module2 {
     public static void main(String[] args) {
         System.out.println("完成模块2的开发");
+        System.out.println("优化了模块2的开发");
     }
 }
